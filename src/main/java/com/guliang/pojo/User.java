a5b9d5bc6ef0e6ecdab3d3d0890cc09f7047a1b9
妃子姬 <3236467502@qq.com>
2017-02-22 11:07:10 +0800

    单独的mybatis框架代码

---
+++ src/main/java/com/guliang/pojo/User.java
@@ -0,0 +1,52 @@
+package com.guliang.pojo;
+
+/**
+ * Created by HuangHailiang on 2017/2/22.
+ */
+public class User {
+    private int id;
+    private String userName;
+    private String userAge;
+    private String userAdress;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserAdress() {
+        return userAdress;
+    }
+
+    public void setUserAdress(String userAdress) {
+        this.userAdress = userAdress;
+    }
+}
